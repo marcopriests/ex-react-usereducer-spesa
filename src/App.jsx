a397149fc7e7a5d7a0bdd1f4df6e2f1e5496567e
marcopriests <prtmrc98@gmail.com>
2025-07-21 20:15:03 +0200

    [UPD] milestone 3 completata

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,53 @@ function App() {
   ];
 
   const addToCart = (product) => {
-    if (!addedProducts.includes(product)) {
+    const existingProduct = addedProducts.find(p => p.name === product.name);
+    if (!existingProduct) {
       setAddedProducts([
         ...addedProducts,
-        {
-          name: product.name,
-          price: product.price,
-          quantity: 1
-        }
-      ])
+        { ...product, quantity: 1 }
+      ]);
+    } else {
+      updateProductQuantity(existingProduct);
     }
+  }
+
+
+  const updateProductQuantity = (product) => {
+    const updatedProducts = addedProducts.map(p => {
+      if (p.name === product.name) {
+        return { ...p, quantity: p.quantity + 1 };
+      }
+      return p;
+    });
+
+    setAddedProducts(updatedProducts);
+  }
+
+  const removeFromCart = (product) => {
+    const p = addedProducts.find(p => p.name === product.name);
+    if (p.quantity === 1) {
+      const updatedCart = addedProducts.filter(p => p.name !== product.name);
+      console.log(updatedCart);
+      setAddedProducts(updatedCart);
+    } else {
+      const updatedProduct = addedProducts.map(p => {
+        if (p.name === product.name) {
+          return { ...p, quantity: p.quantity - 1 };
+        }
+        return p;
+      });
+
+      setAddedProducts(updatedProduct);
+    }
+  }
+
+  const totaleDaPagare = (arr) => {
+    const totale = arr.reduce((acc, product) => {
+      return acc + (product.price * product.quantity);
+    }, 0)
+
+    return totale;
   }
 
   return (
@@ -34,7 +71,7 @@ function App() {
               return (
                 <li className='list-item' key={i}>
                   <p>{p.name}</p>
-                  <p>{p.price}</p>
+                  <p>{p.price.toFixed(2)} €</p>
                   <button className='btn' onClick={() => addToCart(p)}>Aggiungi al carrello</button>
                 </li>
               )
@@ -51,12 +88,16 @@ function App() {
                 return (
                   <li className='list-item' key={i}>
                     <p>{p.name}</p>
-                    <p>{p.price}</p>
+                    <p>{p.price.toFixed(2)} €</p>
                     <p>{p.quantity}</p>
+                    <button className='btn' onClick={() => removeFromCart(p)}>Rimuovi dal carrello</button>
                   </li>
                 )
               })}
             </ul>
+
+            <h4>Totale da pagare: <strong>{totaleDaPagare(addedProducts).toFixed(2)} €</strong></h4>
+
           </div>
         }
       </div>
