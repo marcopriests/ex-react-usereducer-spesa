c08bca3ea7f4bf1f22c8d467a0a3cda7bb753ac7
marcopriests <prtmrc98@gmail.com>
2025-07-21 19:32:38 +0200

    [UPD] milestone 2 completata

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 
 
 function App() {
+  const [addedProducts, setAddedProducts] = useState([]);
+
   const products = [
     { name: 'Mela', price: 0.5 },
     { name: 'Pane', price: 1.2 },
@@ -9,17 +11,55 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  const addToCart = (product) => {
+    if (!addedProducts.includes(product)) {
+      setAddedProducts([
+        ...addedProducts,
+        {
+          name: product.name,
+          price: product.price,
+          quantity: 1
+        }
+      ])
+    }
+  }
+
   return (
     <>
-      <ul>
-        {products.map((p, i) => {
-          return (
-            <li key={i}>
-              <h3>{p.name} - <span>{p.price}</span></h3>
-            </li>
-          )
-        })}
-      </ul>
+      <div className='container'>
+        <div>
+          <h3>Lista spesa</h3>
+          <ul>
+            {products.map((p, i) => {
+              return (
+                <li className='list-item' key={i}>
+                  <p>{p.name}</p>
+                  <p>{p.price}</p>
+                  <button className='btn' onClick={() => addToCart(p)}>Aggiungi al carrello</button>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+        {addedProducts.length != 0
+          &&
+          <div>
+            <h3>Carrello</h3>
+            <ul>
+              {addedProducts.map((p, i) => {
+                return (
+                  <li className='list-item' key={i}>
+                    <p>{p.name}</p>
+                    <p>{p.price}</p>
+                    <p>{p.quantity}</p>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        }
+      </div>
     </>
   )
 }
